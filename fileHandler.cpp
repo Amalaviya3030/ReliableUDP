403b76d892727ffc96e442275453824e703efe94
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-01-21 20:02:29 -0500

    i added two new files and now the create a function in that file

--- fileHandler.cpp
+++ fileHandler.cpp
@@ -1,8 +1,23 @@
+// FileTransfer.cpp
+#include "FileTransfer.h"
 #include <iostream>
-#include <fstream>
-#include <string>
 
-int main(void) {
+bool FileTransfer::SendFileMetadata(const std::string& fileName, size_t fileSize, const std::string& checksum) {
+    // Empty function body
+    return false;
+}
 
+bool FileTransfer::SendFileChunk(const std::vector<unsigned char>& chunk, int chunkIndex) {
+    // Empty function body
+    return false;
+}
 
+bool FileTransfer::ReceiveFileMetadata(std::string& fileName, size_t& fileSize, std::string& checksum) {
+    // Empty function body
+    return false;
+}
+
+bool FileTransfer::ReceiveFileChunk(std::vector<unsigned char>& chunk, int& chunkIndex) {
+    // Empty function body
+    return false;
 }
--- fileHandler.h
+++ fileHandler.h
@@ -1,1 +1,21 @@
-#pragma once
+// FileHandler.h
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+#include <string>
+#include <vector>
+
+class FileHandler {
+public:
+    // Loads a file from disk into a buffer
+    static bool LoadFile(const std::string& filePath, std::vector<unsigned char>& buffer);
+
+    // Saves a buffer to disk as a file
+    static bool SaveFile(const std::string& filePath, const std::vector<unsigned char>& buffer);
+
+    // Verifies the integrity of a file using a checksum
+    static std::string CalculateChecksum(const std::vector<unsigned char>& buffer);
+    static bool VerifyChecksum(const std::vector<unsigned char>& buffer, const std::string& expectedChecksum);
+};
+
+#endif // FILEHANDLER_H
